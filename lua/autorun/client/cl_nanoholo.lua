c25635599d4d5b13bcfb1a19af6c40a10936a4c0
Pierre Leet <ExtReMLapin@users.noreply.github.com>
2014-05-06 13:09:05 +0200

    Code formating, nothing new.

--- lua/autorun/client/cl_nanoholo.lua
+++ lua/autorun/client/cl_nanoholo.lua
@@ -1,5 +1,5 @@
 hook.Add( "PreDrawHalos", "hueheu", function() // Yes, a file only for that
-if LocalPlayer().ZSing then
-halo.Add( ents.FindByClass( "weapon_*" ), Color( 25, 50, 250 ), 0.75, 0.75, 0 )
-end
+    if LocalPlayer().ZSing then
+          halo.Add( ents.FindByClass( "weapon_*" ), Color( 25, 50, 250 ), 0.75, 0.75, 0 )
+    end
 end)
