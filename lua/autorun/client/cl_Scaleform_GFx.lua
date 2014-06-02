ee331a99010369e5af6365a14c31eebd7569cd54
Pierre <ExtReMLapin@users.noreply.github.com>
2014-06-02 17:43:49 +0200

    We've been hacked.
    GREAT

--- lua/autorun/client/cl_Scaleform_GFx.lua
+++ lua/autorun/client/cl_Scaleform_GFx.lua
@@ -1,3 +1,3 @@
 // Extrem-Team.com has been compromised, all of our password, accounts, everything has been compromised, i'm removing that because i had to set a maintenance page
 // God damn, fuck this shit.
- 
+// FUCk 
