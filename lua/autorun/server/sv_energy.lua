aac3e124d143c3b71e750d5e539c7fff81f0e367
Pierre <ExtReMLapin@users.noreply.github.com>
2014-07-12 22:34:47 +0200

    WOOHOOOOOO

--- lua/autorun/server/sv_energy.lua
+++ lua/autorun/server/sv_energy.lua
@@ -11,76 +11,76 @@ local function Regen_PlayerHasSpawneda(ply)
 end
 hook.Add("PlayerSpawn", "Player spawns AR", Regen_PlayerHasSpawneda)
 local function Regen_PlayerTakesDamagea(ply)
-if (!ply:IsPlayer()) then return end
+	if (!ply:IsPlayer()) then return end
 	ply.RDW = CurTime() + (ply.RDW * 0 + 5) // ent.RDW * 0 + (CurTime() + 5)
 	ply.HRAW = 1
 	ply.HRTW = 0
 end
 hook.Add("GryUseEnergy", "Regen Time Penalty After Damage", Regen_PlayerTakesDamagea)
 local function Regen_Doa()
-for _, ply in pairs (player.GetAll()) do
-	if ply:Alive() then
+	for _, ply in pairs (player.GetAll()) do
 		if ply:Alive() then
-			if Regen_PerSect == true then
-			local block
-				if ply:GetNWInt("GryEnergy") > 100 * 0.75 then
-					block = 100
-					elseif ply:GetNWInt("GryEnergy") > 100 * 0.5 then
-block = 100 * 0.75
-elseif ply:GetNWInt("GryEnergy") > 25 then
-block = 100 * 0.5
-else
-block = 25
-end
+			if ply:Alive() then
+				if Regen_PerSect == true then
+					local block
+					if ply:GetNWInt("GryEnergy") > 100 * 0.75 then
+						block = 100
+						elseif ply:GetNWInt("GryEnergy") > 100 * 0.5 then
+							block = 100 * 0.75
+							elseif ply:GetNWInt("GryEnergy") > 25 then
+								block = 100 * 0.5
+							else
+								block = 25
+							end
 
-if Regen_HealRamps == true then
-if CurTime() >= ply.RDW then
-if (ply:GetNWInt("GryEnergy") + ply.HRAW) > block then
-ply:SetNWInt("GryEnergy",block)
-else
-ply:SetNWInt("GryEnergy",math.min(ply:GetNWInt("GryEnergy") + ply.HRAW, block))
-ply.RDW = CurTime() + 0.1
-ply.HRTW = ply.HRTW + 1
-end
-end
-if ply.HRTW >= 3 then
-ply.HRAW = ply.HRAW + 1
-ply.HRTW = 0
-end
-else
-if CurTime() >= ply.RDW then
-ply:SetNWInt("GryEnergy",math.min(ply:GetNWInt("GryEnergy") + 1, block))
-ply.RDW = CurTime() + 0.1
-end
-end
-else
-if Regen_HealRamps == true then
-if ply:GetNWInt("GryEnergy") < 100 then
-if CurTime() >= ply.RDW then
-if (ply:GetNWInt("GryEnergy") + ply.HRAW) > 100 then
-ply:SetNWInt("GryEnergy",100)
-else
-ply:SetNWInt("GryEnergy",ply:GetNWInt("GryEnergy") + ply.HRAW)
-ply.RDW = CurTime() + 0.1
-ply.HRTW = ply.HRTW + 1
-end
-end
-end
-if ply.HRTW >= 3  then
-ply.HRAW = ply.HRAW + 1
-ply.HRTW = 0
-end
-else
-if ply:GetNWInt("GryEnergy") < 100 then
-if CurTime() >= ply.RDW then
-ply:SetNWInt("GryEnergy",ply:GetNWInt("GryEnergy") + 1)
-ply.RDW = CurTime() + 0.1
-end
-end
-end
-end
-end
-end
-end
-end
+							if Regen_HealRamps == true then
+								if CurTime() >= ply.RDW then
+									if (ply:GetNWInt("GryEnergy") + ply.HRAW) > block then
+										ply:SetNWInt("GryEnergy",block)
+									else
+										ply:SetNWInt("GryEnergy",math.min(ply:GetNWInt("GryEnergy") + ply.HRAW, block))
+										ply.RDW = CurTime() + 0.1
+										ply.HRTW = ply.HRTW + 1
+									end
+								end
+								if ply.HRTW >= 3 then
+									ply.HRAW = ply.HRAW + 1
+									ply.HRTW = 0
+								end
+							else
+								if CurTime() >= ply.RDW then
+									ply:SetNWInt("GryEnergy",math.min(ply:GetNWInt("GryEnergy") + 1, block))
+									ply.RDW = CurTime() + 0.1
+								end
+							end
+						else
+							if Regen_HealRamps == true then
+								if ply:GetNWInt("GryEnergy") < 100 then
+									if CurTime() >= ply.RDW then
+										if (ply:GetNWInt("GryEnergy") + ply.HRAW) > 100 then
+											ply:SetNWInt("GryEnergy",100)
+										else
+											ply:SetNWInt("GryEnergy",ply:GetNWInt("GryEnergy") + ply.HRAW)
+											ply.RDW = CurTime() + 0.1
+											ply.HRTW = ply.HRTW + 1
+										end
+									end
+								end
+								if ply.HRTW >= 3  then
+									ply.HRAW = ply.HRAW + 1
+									ply.HRTW = 0
+								end
+							else
+								if ply:GetNWInt("GryEnergy") < 100 then
+									if CurTime() >= ply.RDW then
+										ply:SetNWInt("GryEnergy",ply:GetNWInt("GryEnergy") + 1)
+										ply.RDW = CurTime() + 0.1
+									end
+								end
+							end
+						end
+					end
+				end
+			end
+		end
 hook.Add("Tick", "Player gets his Armor regenerated", Regen_Doa)
