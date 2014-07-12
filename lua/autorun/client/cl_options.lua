8dd7c84b5e36a2f90d0926d56db98b6e65fe3287
Pierre <ExtReMLapin@users.noreply.github.com>
2014-07-12 14:50:06 +0200

    Update cl_options.lua

--- lua/autorun/client/cl_options.lua
+++ lua/autorun/client/cl_options.lua
@@ -1,19 +1,17 @@
 if not GryMod then GryMod = {} end
 
 concommand.Add("GryCredits", function()
-local GryPopup = vgui.Create( "DFrame" )
-GryPopup:SetSize( 1366+4, 768+27 )
-GryPopup:Center()
-GryPopup:SetBackgroundBlur( true )
-GryPopup:MakePopup()
-GryPopup:SetPaintShadow(true)
- 
-local CryDrawFrame = vgui.Create( "HTML", GryPopup )
-CryDrawFrame:SetPos( 2, 25 )
-CryDrawFrame:SetSize( 1366, 768 )
-CryDrawFrame:OpenURL( "http://extrem-team.com/Credits.html" )
- 
-
+	local GryPopup = vgui.Create( "DFrame" )
+			GryPopup:SetSize( 1366+4, 768+27 )
+			GryPopup:Center()
+			GryPopup:SetBackgroundBlur( true )
+			GryPopup:MakePopup()
+			GryPopup:SetPaintShadow(true)
+	 
+	local CryDrawFrame = vgui.Create( "HTML", GryPopup )
+			CryDrawFrame:SetPos( 2, 25 )
+			CryDrawFrame:SetSize( 1366, 768 )
+			CryDrawFrame:OpenURL( "http://extrem-team.com/Credits.html" )
 end)
 
 
@@ -62,32 +60,31 @@ Panel:AddControl("Label", {Text = "GryMod Config"})
 	end
 	
 	
- 
 	local NumSliderThingy = vgui.Create( "DNumSlider", Panel )
 NumSliderThingy:SetPos( 25,150)
-NumSliderThingy:SetSize( 250, 10 ) -- Keep the second number at 100
+NumSliderThingy:SetSize( 250, 10 )
 NumSliderThingy:SetText( "Mirror Distance" )
-NumSliderThingy:SetMin( -5000 ) -- Minimum number of the slider
-NumSliderThingy:SetMax( 5000 ) -- Maximum number of the slider
-NumSliderThingy:SetDecimals( 0 ) -- Sets a decimal. Zero means it's a whole number
-NumSliderThingy:SetConVar( "gry_xadd" ) -- Set the convar
+NumSliderThingy:SetMin( -5000 )
+NumSliderThingy:SetMax( 5000 )
+NumSliderThingy:SetDecimals( 0 )
+NumSliderThingy:SetConVar( "gry_xadd" )
  
 	local NumSliderThingy1 = vgui.Create( "DNumSlider", Panel )
 NumSliderThingy1:SetPos( 25,180)
-NumSliderThingy1:SetSize( 250, 10 ) -- Keep the second number at 100
+NumSliderThingy1:SetSize( 250, 10 ) 
 NumSliderThingy1:SetText( "X Posision" )
-NumSliderThingy1:SetMin( -5000 ) -- Minimum number of the slider
-NumSliderThingy1:SetMax( 5000 ) -- Maximum number of the slider
-NumSliderThingy1:SetDecimals( 0 ) -- Sets a decimal. Zero means it's a whole number
-NumSliderThingy1:SetConVar( "gry_xdist" ) -- Set the convar
+NumSliderThingy1:SetMin( -5000 ) 
+NumSliderThingy1:SetMax( 5000 ) 
+NumSliderThingy1:SetDecimals( 0 ) 
+NumSliderThingy1:SetConVar( "gry_xdist" )
  
  
  local CheckBoxThing = vgui.Create( "DCheckBoxLabel", Panel )
 CheckBoxThing:SetPos( 25,195 )
 CheckBoxThing:SetText( "Eyefinity" )
-CheckBoxThing:SetConVar( "cl_Eyefinity" ) -- ConCommand must be a 1 or 0 value
+CheckBoxThing:SetConVar( "cl_Eyefinity" ) 
 CheckBoxThing:SetValue( 0 )
-CheckBoxThing:SizeToContents() -- Make its size to the contents. Duh?
+CheckBoxThing:SizeToContents() 
  
 Panel:AddItem(disabled1)
 Panel:AddItem(enabled1)
@@ -101,7 +98,6 @@ Panel:AddControl("Button", {Label = "Credits", Text = "tel me who med dis mod pl
 end
 
 function GryMod.GryOptionschuteOptionmenu()
-spawnmenu.AddToolMenuOption( "Options", "GryMod", "Config", "Config it !", "test1", "test23", GryMod.GryOptionschute )
+	spawnmenu.AddToolMenuOption( "Options", "GryMod", "Config", "Config it !", "test1", "test23", GryMod.GryOptionschute )
 end
 hook.Add("PopulateToolMenu", "PopulateToolMenu", GryMod.GryOptionschuteOptionmenu)
-
