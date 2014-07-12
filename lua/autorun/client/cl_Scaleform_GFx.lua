c1a5aa388afaf886101ed510aaab7e6ad3777347
Pierre <ExtReMLapin@users.noreply.github.com>
2014-07-12 14:46:55 +0200

    betar

--- lua/autorun/client/cl_Scaleform_GFx.lua
+++ lua/autorun/client/cl_Scaleform_GFx.lua
@@ -1,21 +1,9 @@
 if not GryMod then GryMod = {} end
-// 22/02/2014 NOTE : IIII FEEEEEEELLLLL THE POWER OF THE POTATOOOOOOOO
---[[ 
- 
- omg srsly, 2 years to code this SHIT
- TWO FUCKING YEARS ?
- 
- 
- TWO ?
- 
- 
- 
- OH MY FUCKING GOD IM SOOOOO LAZY]]
 
  function ScaleFormGFx_Proxy(address, x, y, time)
 	HTMLTest = vgui.Create( "HTML" )
 	HTMLTest:SetPos( ((ScrW()-x)/2),((ScrH()-y)/2) )
-	HTMLTest:SetSize( x+500 , y+500  ) // +500 To prevent the Scrollbar, tell me if you have a way to bypass this ;) And not 5000 to prevent Lag , lel
+	HTMLTest:SetSize( x+500 , y+500  ) -- +500 To prevent the Scrollbar, tell me if you have a way to bypass this ;) And not 5000 to prevent Lag , lel
 	HTMLTest:OpenURL( address )
 		timer.Create( "ScaleFormTimer", time, 1, function()
 			HTMLTest:Remove()
@@ -23,12 +11,11 @@ if not GryMod then GryMod = {} end
  end
 
 
-
  function GryMod.ZoomScaleform()
- GryBinoCulars = vgui.Create( "HTML" )
- GryBinoCulars:SetPos( 0, 0 )
- GryBinoCulars:SetSize( ScrW() , ScrH()  ) //
- GryBinoCulars:OpenURL( "http://extrem-team.com/bino.html" )
+ 	GryBinoCulars = vgui.Create( "HTML" )
+ 	GryBinoCulars:SetPos( 0, 0 )
+	GryBinoCulars:SetSize( ScrW() , ScrH()  ) //
+	GryBinoCulars:OpenURL( "http://extrem-team.com/bino.html" )
  end
 
  function GryMod.DeZoomScaleform()
@@ -36,8 +23,8 @@ if !IsValid(GryBinoCulars) then return end
 	GryBinoCulars:Clear() // Maybe useless ... who know ? :V
 	GryBinoCulars:Stop()
 	GryBinoCulars:Remove()
- end
- 
- net.Receive("gry_spawn", function()
-ScaleFormGFx_Proxy("http://extrem-team.com/init.html", 1000, 700, 5) // Rip cheap monitors
- end)
+end
+
+net.Receive("gry_spawn", function()
+	ScaleFormGFx_Proxy("http://extrem-team.com/init.html", 1000, 700, 5) -- Rip cheap monitors
+end)
