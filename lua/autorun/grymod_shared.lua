818b6b8dfcb5f209b83e77776788b9e64f62d046
Lapin <ExtReMLapin@users.noreply.github.com>
2014-11-06 18:22:27 +0100

    im bored

--- lua/autorun/grymod_shared.lua
+++ lua/autorun/grymod_shared.lua
@@ -49,15 +49,15 @@ function constrain(val, minv, maxv)
 end
 
 if SERVER then
-AddCSLuaFile( "client/cl_hud.lua" )
-AddCSLuaFile( "client/cl_fonts.lua" )
-AddCSLuaFile( "client/cl_grenade.lua" )
-AddCSLuaFile( "client/cl_allycross.lua" )
-AddCSLuaFile( "client/cl_nanoholo.lua" )
-AddCSLuaFile( "client/cl_options.lua" )
-AddCSLuaFile( "client/cl_Scaleform_GFx.lua" )
-AddCSLuaFile( "sh_crashlogo.lua" )
-AddCSLuaFile( )
+	AddCSLuaFile( "client/cl_hud.lua" )
+	AddCSLuaFile( "client/cl_fonts.lua" )
+	AddCSLuaFile( "client/cl_grenade.lua" )
+	AddCSLuaFile( "client/cl_allycross.lua" )
+	AddCSLuaFile( "client/cl_nanoholo.lua" )
+	AddCSLuaFile( "client/cl_options.lua" )
+	AddCSLuaFile( "client/cl_Scaleform_GFx.lua" )
+	AddCSLuaFile( "sh_crashlogo.lua" )
+	AddCSLuaFile( )
 end
 
 if (SERVER) then
